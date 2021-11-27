d8b2f101508ef8273c1b80ce60016b10db87fdb0
Carcione Christian <carcione.christian@gmail.com>
2021-11-27 22:33:04 +0100

    add user

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -2,13 +2,18 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
 using IdentityServer4.Services;
+using IdentityServer4.Test;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -34,7 +39,7 @@ namespace IdentityServer
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
-                .AddTestUsers(TestUsers.Users)
+                .AddTestUsers(TestUsers.Users.Concat(GetUser()).ToList())
                 ;
 
             // not recommended for production - you need to store your key material somewhere secure
@@ -70,5 +75,39 @@ namespace IdentityServer
                 endpoints.MapDefaultControllerRoute();
             });
         }
+
+        private List<TestUser> GetUser() =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "918727",
+                    Username = "Luana@email.com",
+                    Password = "Luana",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Luana Pasticceria"),
+                        new Claim(JwtClaimTypes.GivenName, "Luana"),
+                        new Claim(JwtClaimTypes.FamilyName, "Pasticceria"),
+                        new Claim(JwtClaimTypes.Email, "LuanaPasticceria@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "918727",
+                    Username = "Maria@email.com",
+                    Password = "Maria",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Maria Pasticceria"),
+                        new Claim(JwtClaimTypes.GivenName, "Maria"),
+                        new Claim(JwtClaimTypes.FamilyName, "Pasticceria"),
+                        new Claim(JwtClaimTypes.Email, "LuanaPasticceria@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                    }
+                },
+            };
+
     }
 }
