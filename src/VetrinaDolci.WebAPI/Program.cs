18f1def4f0652e915745d16bbffe3ce7bb7e687c
Carcione Christian <carcione.christian@gmail.com>
2021-11-28 02:52:12 +0100

    log console web.api

--- src/VetrinaDolci.WebAPI/Program.cs
+++ src/VetrinaDolci.WebAPI/Program.cs
@@ -18,6 +18,11 @@ namespace VetrinaDolci.WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
