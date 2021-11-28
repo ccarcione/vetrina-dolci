f2082a1083ccc4e97650eed691f0a309416cb56d
Carcione Christian <carcione.christian@gmail.com>
2021-11-28 23:15:04 +0100

    fix seed data da csv

---
+++ src/VetrinaDolci.WebAPI/Controllers/SetupController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VetrinaDolci.WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SetupController : ControllerBase
+    {
+        private readonly ApplicationContext _applicationContext;
+
+        public SetupController(ApplicationContext applicationContext)
+        {
+            this._applicationContext = applicationContext;
+        }
+
+        // GET: api/<SetupController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            if (_applicationContext.Dolci.Any()
+                || _applicationContext.Ingredienti.Any())
+            {
+                return StatusCode(410, "seed already done");
+            }
+
+            //seed here
+            await SeedHelper.SeedFromCsv(_applicationContext);
+
+            return Ok("seed done");
+        }
+    }
+}
